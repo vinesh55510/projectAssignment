6c2d0326b0610d72c1e203a41e4111eb49839442
vinesh55510 <vinesh@ensarsolutions.com>
2023-05-24 11:06:53 +0530

    project-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+interface stateType {
+  name: { title: string; first: string; last: string };
+  email: string;
+}
+
+const App = () => {
+  const [state, setState] = useState<stateType[]>([] as any[]);
+
+  const refresh = () => {
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    async function getStorageData() {
+      localStorage.setItem("items", JSON.stringify(state));
+      const response = await axios.get("https://randomuser.me/api");
+      console.log(response.data.results);
+      setState(response.data.results);
+    }
+    getStorageData();
+  }, [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        {state.map((item) => (
+          <div>
+            <span>{item.name.title}</span> &nbsp;
+            <span>{item.name.first}</span> &nbsp;
+            <span>{item.name.last}</span> <br />
+            <span>{item.email}</span>
+          </div>
+        ))}
+        <button onClick={refresh}>Refresh</button>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
